6037a4a792126d55339eebb1dfce483b2e56f67b
berriosa13 <berriosa13@gmail.com>
2023-04-07 19:42:07 -0400

    Added authorized routing to pokedex page

--- pages/index.js
+++ pages/index.js
@@ -10,55 +10,62 @@ import Link from 'next/link'
 function HomePage() {
   const { data: session } = useSession();
   
-  function handleSignOut(){
-    signOut();
-  }
   
   return (
     <div>
       <Head>
-        <title>Home Page</title>
+        <title>Pokédex-HomePage</title>
       </Head>
 
       {session ? AuthorizedUser({ session, handleSignOut }) : UnauthorizedUser()}
     </div>
   )
 }
-export default HomePage;
-
-function UnauthorizedUser(){
+export default HomePage
+function UnauthorizedUser() {
   const router = useRouter()
   useEffect(() => {
     router.replace('/login')
   },[router])
-
+  
   return null;
 }
 
 function AuthorizedUser({ session, handleSignOut }) {
-  return(
-    <main className="container mx-auto text-center py-20">
-          <h3 className='text-4xl font-bold'>Authorize User Homepage</h3>
+  
+  const router = useRouter()
+  useEffect(() => {
+    router.replace('/pokedex')
+  },[router])
+  
+  return null;
+  // return(
+    //   <main className="container mx-auto text-center py-20">
+    //         <h3 className='text-4xl font-bold'>Authorize User Homepage</h3>
+    
+    //         <div className='details'>
+    //         <h5>{session.user.name}</h5>
+  //         <h5>{session.user.email}</h5>
+  //         </div>
+  
+  //         <div className="flex justify-center">
+  //           <button onClick={handleSignOut} className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 bg-gray-50'>Sign Out</button>
+  //         </div>
+  
+  //         <div className='flex justify-center'>
+  //           <Link href={'/pokedex'}><a className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50'>Pokedex</a></Link>
+  //         </div>
+  //     </main>
+  // )
+}
 
-          <div className='details'>
-          <h5>{session.user.name}</h5>
-          <h5>{session.user.email}</h5>
-          </div>
-
-          <div className="flex justify-center">
-            <button onClick={handleSignOut} className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 bg-gray-50'>Sign Out</button>
-          </div>
-
-          <div className='flex justify-center'>
-            <Link href={'/pokedex'}><a className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50'>Pokedex</a></Link>
-          </div>
-      </main>
-  )
+function handleSignOut(){
+  signOut();
 }
 
 export async function getServerSideProps({ req }){
   const session = await getSession({ req })
-
+  
   if(!session){
     return {
       redirect : {
